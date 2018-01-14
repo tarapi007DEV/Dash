01621a586bd86902513c0833bbe36752310e7d23
clickticonsultoria <clickticonsultoria@gmail.com>
2018-01-14 06:19:04 -0200

    sei la o que;

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -9,35 +9,37 @@ function deumerda() {
     console.log("deu merda na request");
 }
 
-var onClickBtnLogin = function () {
-    $.ajax({
-        url: "LoginServlet",
-        method: "post",
-        success: function (data, textStatus, jqXHR) {
-            try {
-                if (data == "1") {
-                    console.log("sucesso");
-                } else {
-                    deumerda();
-                }
-            } catch (e) {
-                console.log("deu exception");
-                console.log(e);
-            }
-
-        },
-        error: function () {
-            deumerda();
-        }
-    });
+var sendLoginRequest = function () {
+//    $.ajax({
+//        url: "LoginServlet",
+//        method: "post",
+//        success: function (data, textStatus, jqXHR) {
+//            try {
+//                if (data == "1") {
+//                    console.log("sucesso");
+//                } else {
+//                    deumerda();
+//                }
+//            } catch (e) {
+//                console.log("deu exception");
+//                console.log(e);
+//            }
+//
+//        },
+//        error: function () {
+//            deumerda();
+//        }
+//    });
+    location.href = "index.html";
 };
 $(".btn-login").click(function () {
+    /* TODO MODAL DE AGUARDE*/
     var usuario = $(".usuario").val();
     var senha = $(".senha").val();
     if ("" == usuario || "" == senha) {
         console.log("nulos");
     } else {
-        onClickBtnLogin();
+        sendLoginRequest();
     }
 }
 );
